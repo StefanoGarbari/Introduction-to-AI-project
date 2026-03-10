31b0436586620345ca96991e192af8fdca0fba2b
StefanoGarbari <stefano.garbari3@gmail.com>
2026-03-10 14:47:21 +0100

    Add basic logic of the game and example random player

--- tsuro.py
+++ tsuro.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from random import shuffle
+from random import shuffle, randint, choice
 from tiles import TILES
 import pygame
 from abc import ABC, abstractmethod
@@ -19,21 +19,23 @@ class PlacedTile:
     rotation: int
 
 class Player(ABC):
-    start: Position
-    hand : list[Tile] = []
-
     def __init__(self):
-        self.hand = []
-        self.start = None
+        self.hand: list[Tile]= []
+        self.start: Position= None
 
     @abstractmethod
-    def play(self, state: State):
+    def choose_action(self, state: State) -> Action:
         """
         Play using this strategy
         """
         pass
 
 Board = list[list[PlacedTile | None]]
+
+@dataclass
+class Action:
+    player: Player
+    placed_tile: PlacedTile
 
 class State:
     def __init__(self, players: list[Player], N=6):
@@ -47,8 +49,9 @@ class State:
             for _ in range(3):
                 player.hand.append(self.draw_pile.pop())
 
+        self.active_player = 0
 
-    def follow_path(self, start: Position) -> tuple[Position, bool]:
+    def follow_path(self, start: Position) -> tuple[Position, Position, bool]:
         # entry: (di, dj, new_entry)
         ENTRY_MAP = {
             1: (1, 0, 6),
@@ -67,13 +70,15 @@ class State:
         i = start.i + di
         j = start.j + dj
 
+        next_pos = Position(i=i, j=j, entry=entry)
+
         # check if the new position is outside of the board
         if i < 0 or i >= len(self.board) or j < 0 or j >= len(self.board[0]):
-            return Position(i=i, j=j, entry=entry), False
-        
+            return start, next_pos, False
+
         # check if the new position doesn't contain a tile
         if self.board[i][j] is None:
-            return start, True
+            return start, next_pos, True
         
 
         # rotate the entry point instead of the tile (easier)
@@ -91,24 +96,114 @@ class State:
 
         return self.follow_path(Position(i=i, j=j, entry=exit))
 
+    def is_player_alive(self, player: Player):
+        return self.follow_path(player.start)[2]
 
-state = State([])
+    def apply(self, action: Action):
+        # remove the tile from the player's hand
+        action.player.hand.remove(action.placed_tile.tile)
 
-# test tiles 
-state.board[0][0] = PlacedTile(TILES[2], 0)
-state.board[0][1] = PlacedTile(TILES[8], 2)
-state.board[1][1] = PlacedTile(TILES[3], 2)
-state.board[2][1] = PlacedTile(TILES[12], 3)
-state.board[2][0] = PlacedTile(TILES[13], 1)
+        # place the tile on the board
+        _, next_pos, _ = self.follow_path(action.player.start)
+        self.board[next_pos.i][next_pos.j] = action.placed_tile
 
-state.board[5][3] = PlacedTile(TILES[30], 3)
-state.board[4][3] = PlacedTile(TILES[25], 3)
-state.board[4][2] = PlacedTile(TILES[34], 0)
+        # player draws a new tile
+        if self.draw_pile:
+            action.player.hand.append(self.draw_pile.pop())
 
-pos = state.follow_path(Position(0,-1,3))
-print(pos)
+        # if any player was killed, put its tiles back in the draw pile
+        for p in self.players:
+            if not self.is_player_alive(p):
+                self.draw_pile.extend(p.hand)
+                p.hand.clear()
+
+        # update next player
+        # if all are dead, there is no active player
+        if all(not self.is_player_alive(p) for p in self.players):
+            self.active_player = None
+            return
+        i = (self.active_player+1) % len(self.players)
+        # skip players who already lost
+        while not self.is_player_alive(self.players[i]):
+            i = (i+1) % len(self.players)
+        self.active_player = i
+
+    def actions(self) -> list[Action]:
+        p = self.players[self.active_player]
+
+        # position where the tile will be placed
+        _, next_pos, _ = self.follow_path(p.start)
+
+        legal_actions = []
+        illegal_actions = []
+
+        for tile in p.hand:
+            for rotation in range(4):
+                placed = PlacedTile(tile, rotation)
+
+                # simulate
+                self.board[next_pos.i][next_pos.j] = placed
+                if self.is_player_alive(p):
+                    legal_actions.append(Action(p, placed))
+                else:
+                    illegal_actions.append(Action(p, placed))
+
+                # undo
+                self.board[next_pos.i][next_pos.j] = None
+
+        return legal_actions if legal_actions else illegal_actions
 
 
+class Game:
+    def __init__(self, players: list[Player]):
+        self.state = State(players)
+
+    def play(self):
+        # choose starting point at random for now
+        height = len(self.state.board)
+        width = len(self.state.board[0])
+        for player in self.state.players:
+            ran = randint(0, 2 * height + 2 * width - 1)
+
+            if ran < width:                     # top edge
+                i = -1
+                j = ran
+                entry = randint(1, 2)
+            elif ran < width + height:          # right edge
+                i = ran - width
+                j = width
+                entry = randint(7, 8)
+            elif ran < 2 * width + height:      # bottom edge
+                i = height
+                j = ran - (width + height)
+                entry = randint(5, 6)
+            else:                               # left edge
+                i = ran - (2 * width + height)
+                j = -1
+                entry = randint(3, 4)
+
+            player.start = Position(i, j, entry)
+
+        # play in loop
+        while ( sum(self.state.is_player_alive(p) for p in self.state.players) >= 2 and
+                sum(t is not None for row in self.state.board for t in row) < height * width -1 ):
+            
+            active_player = self.state.players[self.state.active_player]
+            action = active_player.choose_action(self.state)
+            self.state.apply(action)
+
+
+
+
+class RandomPlayer(Player):
+
+    def choose_action(self, state):
+        return choice(state.actions())
+
+p1 = RandomPlayer()
+p2 = RandomPlayer()
+game = Game([p1, p2])
+game.play()
 
 
 ###################### DEBUG GRAPHICS ########################
@@ -117,14 +212,15 @@ print(pos)
 
 pygame.init()
 TILE_SIZE = 100
-HEIGHT = len(state.board) * TILE_SIZE
-WIDTH = len(state.board[0]) * TILE_SIZE
+HEIGHT = len(game.state.board) * TILE_SIZE
+WIDTH = len(game.state.board[0]) * TILE_SIZE
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tile Board")
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (50, 100, 255)
+RED = (255, 100, 0)
 
 # ----------------------------
 # Entry position calculation
@@ -168,12 +264,12 @@ def get_entry_point(i: int, j: int, entry: int, rotation: int):
 def draw_board():
     screen.fill(WHITE)
 
-    for i in range(len(state.board)):
-        for j in range(len(state.board[0])):
+    for i in range(len(game.state.board)):
+        for j in range(len(game.state.board[0])):
             rect = pygame.Rect(j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE)
             pygame.draw.rect(screen, BLACK, rect, 1)
 
-            tile = state.board[i][j]
+            tile = game.state.board[i][j]
             if tile:
                 drawn = set()
 
@@ -188,7 +284,59 @@ def draw_board():
 
                     drawn.add(a)
                     drawn.add(b)
+    
+    def draw_path(state: State, start: Position):
+        ENTRY_MAP = {
+            1: (1, 0, 6),
+            2: (1, 0, 5),
+            3: (0, 1, 8),
+            4: (0, 1, 7),
+            5: (-1, 0, 2),
+            6: (-1, 0, 1),
+            7: (0, -1, 4),
+            8: (0, -1, 3),
+        }
 
+        # calculate coordinates i,j of the next tile
+        # calculate entry point of the next tile
+        di, dj, entry = ENTRY_MAP[start.entry]
+        i = start.i + di
+        j = start.j + dj
+
+        next_pos = Position(i=i, j=j, entry=entry)
+
+        # check if the new position is outside of the board
+        if i < 0 or i >= len(state.board) or j < 0 or j >= len(state.board[0]):
+            return
+
+        # check if the new position doesn't contain a tile
+        if state.board[i][j] is None:
+            return
+
+        # rotate the entry point instead of the tile (easier)
+        entry_rotated = entry - 2 * state.board[i][j].rotation
+        if entry_rotated <= 0:
+            entry_rotated += 8
+
+        # follow tile path
+        exit_rotated = state.board[i][j].tile[entry_rotated]
+
+        # rotate back the exit
+        exit = exit_rotated + 2 * state.board[i][j].rotation
+        if exit > 8:
+            exit -= 8
+        
+        
+        p1 = get_entry_point(i, j, entry, 0)
+        p2 = get_entry_point(i, j, exit, 0)
+
+        pygame.draw.line(screen, RED, p1, p2, 3)
+
+        draw_path(state, Position(i=i, j=j, entry=exit))
+
+    for p in game.state.players:
+        draw_path(game.state, p.start)
+        
 
 # ----------------------------
 # Main loop
